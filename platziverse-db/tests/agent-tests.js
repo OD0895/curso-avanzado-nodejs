fcff6c76f40b60270ab8e3cdc1075846cf1443b9
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-08-16 17:42:21 -0500

    :white_check_mark: Implementación de createOrUpdate

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -1,63 +1,101 @@
-'use strict'
+"use strict";
 
-const test = require('ava')
-const sinon = require('sinon')
-const proxyquire = require('proxyquire')
+const test = require("ava");
+const sinon = require("sinon");
+const proxyquire = require("proxyquire");
 
-const agentFixtures = require('./fixtures/agent');
+const agentFixtures = require("./fixtures/agent");
 
 let config = {
-  logging () {}
-}
+  logging() {},
+};
 
 let MetricStub = {
-  belongsTo: sinon.spy()
-}
+  belongsTo: sinon.spy(),
+};
 
 let single = Object.assign({}, agentFixtures.single);
 let id = 1;
-let AgentStub = null
-let db = null
-let sandbox = null
+let uuid = "yyy-yyy-yyy";
+let AgentStub = null;
+let db = null;
+let sandbox = null;
+
+let uuidArgs = {
+  where: {
+    uuid,
+  },
+};
 
 test.beforeEach(async () => {
-  sandbox = sinon.createSandbox()
+  sandbox = sinon.createSandbox();
 
   AgentStub = {
-    hasMany: sandbox.spy()
-  }
+    hasMany: sandbox.spy(),
+  };
+
+  //Model findOne Stub
+  AgentStub.findOne = sandbox.stub();
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)));
 
   //Model findById Stub
   AgentStub.findById = sandbox.stub();
-  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)));
+  AgentStub.findById
+    .withArgs(id)
+    .returns(Promise.resolve(agentFixtures.byId(id)));
 
-  const setupDatabase = proxyquire('../', {
-    './models/agent': () => AgentStub,
-    './models/metric': () => MetricStub
-  })
+  //Model update Stub
+  AgentStub.update = sandbox.stub();
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single));
 
-  db = await setupDatabase(config)
-})
+  const setupDatabase = proxyquire("../", {
+    "./models/agent": () => AgentStub,
+    "./models/metric": () => MetricStub,
+  });
 
-test.afterEach(() => {
-  sandbox && sinon.restore()
-})
-
-test('Agent', t => {
-  t.truthy(db.Agent, 'Agent service should exist')
-})
-
-test.serial('Setup', t => {
-  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
-  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
-  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
-  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+  db = await setupDatabase(config);
 });
 
-test.serial('Agent#findById', async t => {
+test.afterEach(() => {
+  sandbox && sinon.restore();
+});
+
+test("Agent", (t) => {
+  t.truthy(db.Agent, "Agent service should exist");
+});
+
+test.serial("Setup", (t) => {
+  t.true(AgentStub.hasMany.called, "AgentModel.hasMany was executed");
+  t.true(
+    AgentStub.hasMany.calledWith(MetricStub),
+    "Argument should be the MetricModel"
+  );
+  t.true(MetricStub.belongsTo.called, "MetricModel.belongsTo was executed");
+  t.true(
+    MetricStub.belongsTo.calledWith(AgentStub),
+    "Argument should be the AgentModel"
+  );
+});
+
+test.serial("Agent#findById", async (t) => {
   let agent = await db.Agent.findById(id);
-  t.true(AgentStub.findById.called, 'AgentModel.findById was executed');
-  t.true(AgentStub.findById.calledOnce, 'AgentModel.findById was executed once');
-  t.true(AgentStub.findById.calledWith(id), 'AgentModel.findById was executed with id');
-  t.deepEqual(agent, agentFixtures.byId(id), 'Should be the same');
+  t.true(AgentStub.findById.called, "AgentModel.findById was executed");
+  t.true(
+    AgentStub.findById.calledOnce,
+    "AgentModel.findById was executed once"
+  );
+  t.true(
+    AgentStub.findById.calledWith(id),
+    "AgentModel.findById was executed with id"
+  );
+  t.deepEqual(agent, agentFixtures.byId(id), "Should be the same");
+});
+
+test.serial("Agent#createOrUpdate - existe", async (t) => {
+  let agent = await db.Agent.createOrUpdate(single);
+  t.true(AgentStub.findOne.called, "findOne should be called on model");
+  t.true(AgentStub.findOne.calledTwice, "findOne should be called twice");
+  t.true(AgentStub.update.calledOnce, "update should be called once");
+
+  t.deepEqual(agent, single, "Agent hould be the same");
 });
